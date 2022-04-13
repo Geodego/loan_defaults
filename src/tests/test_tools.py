9ca10a12026f90ca44503c0282d05291190e05f0
Geodego <geoffroydegournay@hotmail.com>
2022-04-13 18:15:31 +0200

    feat: Creation of test_tools.py

---
+++ src/tests/test_tools.py
@@ -0,0 +1,19 @@
+from src.tools import get_path_root, root_directory, get_absolute_path
+
+
+def test_get_path_root():
+    """
+    Test that path returned is the absolut path to the project directory
+    """
+    path_root = get_path_root()
+    assert path_root.name == root_directory
+    assert path_root.is_absolute()
+
+
+def test_get_absolute_path():
+    local_path = "data/accounts.csv"
+    absolute_path = get_absolute_path(local_path)
+    assert absolute_path.is_absolute()
+    assert absolute_path.name == 'accounts.csv'
+    assert absolute_path.parents[1].name == root_directory
+
