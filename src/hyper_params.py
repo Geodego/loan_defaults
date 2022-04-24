6611ec1cb83a793132b6acb2a70a5147c0faba88
Geodego <geoffroydegournay@hotmail.com>
2022-04-24 07:42:06 +0200

    feat: Development of hyper_params.py

--- src/hyper_params.py
+++ src/hyper_params.py
@@ -1,5 +1,5 @@
 """
-This file is used to define the objective function used by Optuna when fine-tuning hyper-parameters.
+This file is used to define the objective function used by Optuna for fine-tuning hyperparameters.
 """
 
 import numpy as np
@@ -11,10 +11,17 @@ from lightgbm import log_evaluation
 from sklearn.metrics import mean_squared_error
 
 
-def objective(trial: Trial, train_x: pd.DataFrame, test_x: pd.DataFrame,
-              train_y: np.array, test_y: np.array) -> float:
+def objective(trial: Trial, train_x: pd.DataFrame, val_x: pd.DataFrame,
+              train_y: np.array, val_y: np.array) -> float:
     """
     Optimisation function using Optuna. The objective of our function is to minimize the RMSE
+    :param trial: Trial Optuna object
+    :param train_x: features used for training
+    :param val_x: features used for validation
+    :param train_y: target variable for training
+    :param val_y: trarget variable for validation
+    :return:
+    rmse
     """
     param = {
         'metric': 'rmse',
@@ -25,21 +32,27 @@ def objective(trial: Trial, train_x: pd.DataFrame, test_x: pd.DataFrame,
         'colsample_bytree': trial.suggest_categorical('colsample_bytree', [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]),
         'subsample': trial.suggest_categorical('subsample', [0.4, 0.5, 0.6, 0.7, 0.8, 1.0]),
         'learning_rate': trial.suggest_categorical('learning_rate', [0.006, 0.008, 0.01, 0.014, 0.017, 0.02]),
-        'max_depth': trial.suggest_categorical('max_depth', [3, 5, 7, 10, 12, 15]),
-        'num_leaves': trial.suggest_int('num_leaves', 1, 1000),
+        'max_depth': trial.suggest_categorical('max_depth', [2, 3, 5, 7, 10]),
+        'num_leaves': trial.suggest_int('num_leaves', 2, 100),
         'min_child_samples': trial.suggest_int('min_child_samples', 1, 300),
-        'cat_smooth': trial.suggest_int('min_data_per_groups', 1, 100)
+        #'cat_smooth': trial.suggest_int('min_data_per_groups', 1, 100)
     }
     model = LGBMRegressor(**param)
-    model.fit(train_x, train_y, eval_set=[(test_x, test_y)], callbacks=[early_stopping(100), log_evaluation(100)])
-    preds = model.predict(test_x)
-    rmse = mean_squared_error(test_y, preds, squared=False)
+    model.fit(train_x, train_y, eval_set=[(val_x, val_y)], callbacks=[early_stopping(100), log_evaluation(100)])
+    preds = model.predict(val_x)
+    rmse = mean_squared_error(val_y, preds, squared=False)
 
     return rmse
 
 
-def get_objective(train_x: pd.DataFrame, test_x: pd.DataFrame, train_y: np.array, test_y: np.array):
+def get_objective(train_x: pd.DataFrame, val_x: pd.DataFrame, train_y: np.array, val_y: np.array):
     """
-    Returns the objective function as used by optuna given our dataset. The function returned is a function of trial.
+    Returns the optimization function as used by optuna given our dataset. The function returned is a function of trial.
+    :param train_x: features used for training
+    :param val_x: features used for validation
+    :param train_y: target variable for training
+    :param val_y: trarget variable for validation
+    :return:
+    optimization function
     """
-    return lambda trial: objective(trial, train_x, test_x, train_y, test_y)
+    return lambda trial: objective(trial, train_x, val_x, train_y, val_y)
