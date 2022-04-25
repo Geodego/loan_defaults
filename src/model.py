730eb8a1268128348bb21a36f7b290c12e71d7b2
Geodego <geoffroydegournay@hotmail.com>
2022-04-25 10:40:37 +0200

    docs: Comments completed

--- src/model.py
+++ src/model.py
@@ -1,15 +1,17 @@
 """
 Library of methods for model manipulation.
 """
+import pandas as pd
 
 from .tools import get_absolute_path
 from .data_tools import process_data
 from joblib import dump, load
+from lightgbm import LGBMRegressor
 
 
-def save_model(model, file_name):
+def save_model(model: LGBMRegressor, file_name: str):
     """
-
+    Save the model as a joblib file in folder models. If file_name='lgb', save as 'lgb.joblib'
     :param model:
     :param file_name:  ex 'lgb'
     :return:
@@ -18,17 +20,21 @@ def save_model(model, file_name):
     dump(model, get_absolute_path(file_path))
 
 
-def load_model(file_name):
+def load_model(file_name: str) -> LGBMRegressor:
+    """
+    Load the model saved in folder models with the name file_name
+    :param file_name: name of the file where is saved the model (ex: 'lgb')
+    """
     file_path = 'models/' + file_name + '.joblib'
     model = load(get_absolute_path(file_path))
     return model
 
 
-def predict(account, transaction):
-    """make prediction given input of the API"""
+def predict(account: pd.DataFrame, transaction: pd.DataFrame) -> float:
+    """
+    Make a prediction of the next month outgoing given input of the API
+    """
     template = process_data(account, transaction)
     model = load_model('lgb')
     prediction = model.predict(template)[0]
     return prediction
-
-
